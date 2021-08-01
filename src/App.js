135e8b101046be94d366d0dac7276992cb805e9a
Vijay Kumar <aasiyavijay@gmail.com>
2021-08-01 10:18:21 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        React Firebase Demo
+      </header>
+    </div>
+  );
+}
+
+export default App;
